4c5e227e2d723c66ffc58b13190a0963cc45cc1a
suryaraor <suryaraor@users.noreply.github.com>
2023-10-15 15:28:34 -0500

    surya-example-bucket-004

--- main.tf
+++ main.tf
@@ -1,4 +1,29 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "surya-aws-terraform-bucket-002"
-  acl    = "private"
+resource "aws_s3_bucket" "surya-example-bucket-004" {
+  bucket = "my-tf-surya-example-bucket-004-bucket"
+}
+
+resource "aws_s3_bucket_ownership_controls" "surya-example-bucket-004" {
+  bucket = aws_s3_bucket.surya-example-bucket-004.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "surya-example-bucket-004" {
+  bucket = aws_s3_bucket.surya-example-bucket-004.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "surya-example-bucket-004" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.surya-example-bucket-004,
+    aws_s3_bucket_public_access_block.surya-example-bucket-004,
+  ]
+
+  bucket = aws_s3_bucket.surya-example-bucket-004.id
+  acl    = "public-read"
 }
