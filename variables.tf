554a7f882e2f433594fb0e6d48c844c9fb7e7a7e
suryaraor <suryaraor@users.noreply.github.com>
2023-10-15 13:20:48 -0500

    defined a new variable AWS_SECRET_ACCESS_KEY

--- variables.tf
+++ variables.tf
@@ -3,3 +3,7 @@ variable "aws_region" {
   description = "(Optional) AWS Region to use. Default: us-east-1"
   default     = "us-east-1"
 }
+variable "AWS_SECRET_ACCESS_KEY" {
+  type        = string
+  description = "AWS_SECRET_ACCESS_KEY"
+}
